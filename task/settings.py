166e1f1d5c5add08100cac9cc8d86e0f1f4bdc6b
Anton Chusovitin <antonion2011@yandex.ru>
2019-01-19 16:27:07 +0700

    * Добавил конфиги для базы данных

--- task/settings.py
+++ task/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ARM',
+        'USER': 'ARM',
+        'PASSWORD': 'password',
+        'HOST': '90.189.132.25',
+        'PORT': '5432',
     }
 }
 
