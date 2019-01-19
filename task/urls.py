563191d8fd7e24dcd1379f002dc58a47d50c5fbf
Roman F <roman_11_11_11@mail.ru>
2019-01-20 00:17:12 +0700

    Добавлен метод установки сессии Добавлены новые константы для кодирования answer и session

--- task/urls.py
+++ task/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task_lesson.api.authorization.get_session import Authorization as Auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', Auth.get_session)
 ]
