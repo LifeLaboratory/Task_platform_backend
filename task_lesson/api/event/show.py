02e90302548822501e4d9b3308bd9800a280d5ae
Anton Chusovitin <antonion2011@yandex.ru>
2019-01-20 06:00:43 +0700

    * Фикс отображения событий * Фикс модели

--- task_lesson/api/event/show.py
+++ task_lesson/api/event/show.py
@@ -10,5 +10,12 @@ from django.db.models import Count
 class ShowEvent:
     @staticmethod
     def get_list_event(response):
-        all_event = [event.event for event in Event.objects.all()]
-        return HttpResponse(json.loads({names.ANSWER: all_event}))
+        all_event = list()
+        for event in Event.objects.all():
+            all_event.append({
+                names.EVENT: event.event,
+                names.EVENT_NAME: event.name,
+                names.EVENT_PICTURE_URL: event.pictureurl,
+                names.EVENT_DESC: event.description
+            })
+        return HttpResponse(json.dumps({names.ANSWER: all_event}))
