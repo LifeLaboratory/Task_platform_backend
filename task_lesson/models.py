a561afa32665e0e04d1df5eda2cdb30c0b814603
Anton Chusovitin <antonion2011@yandex.ru>
2019-01-19 19:08:23 +0700

    * фикс модели

--- task_lesson/models.py
+++ task_lesson/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class User(models.Model):
     user = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
-    login = models.CharField(max_length=128)
-    password = models.CharField(max_length=128)
-    email = models.CharField(max_length=128)
+    name = models.TextField(max_length=128, default='', null=False)
+    login = models.TextField(max_length=128, default='', null=False)
+    password = models.TextField(max_length=128, default='', null=False)
+    email = models.TextField(max_length=128, default='', null=False)
 
     def __str__(self):
         return self.user
@@ -16,28 +16,28 @@ class User(models.Model):
 
 class Role(models.Model):
     role = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
+    name = models.TextField(max_length=128, default='', null=False)
 
 
 class Team(models.Model):
     team = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
+    name = models.TextField(max_length=128, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pictureurl = models.CharField(max_length=128)
+    pictureurl = models.TextField(max_length=128, default='', null=False)
 
 
 class TeamUser(models.Model):
     teamuser = models.AutoField(primary_key=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.BooleanField(default=None)
+    role = models.BooleanField(default=None, null=False)
 
 
 class Event(models.Model):
     event = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
-    pictureurl = models.CharField(max_length=128)
-    description = models.CharField(max_length=10000)
+    name = models.TextField(max_length=128, default='', null=False)
+    pictureurl = models.TextField(max_length=128, default='', null=False)
+    description = models.TextField(max_length=10000, default='', null=False)
 
 
 class EventTeamUser(models.Model):
@@ -49,11 +49,11 @@ class EventTeamUser(models.Model):
 
 class Task(models.Model):
     task = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
-    category = models.CharField(max_length=128)
-    weight = models.FloatField(max_length=128)
-    flag = models.CharField(max_length=128)
-    description = models.CharField(max_length=10000)
+    name = models.TextField(max_length=128, default='', null=False)
+    category = models.TextField(max_length=128, default='', null=False)
+    weight = models.FloatField(max_length=128, default=0.0, null=False)
+    flag = models.TextField(max_length=128, default='', null=False)
+    description = models.TextField(max_length=10000, default='', null=False)
     # Автор задания
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -66,8 +66,8 @@ class EventTask(models.Model):
 
 class Sponsor(models.Model):
     sponsor = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
-    pictureurl = models.CharField(max_length=128)
+    name = models.TextField(max_length=128, null=False)
+    pictureurl = models.TextField(max_length=128, default='', null=False)
 
 
 class EventSponsor(models.Model):
@@ -79,11 +79,11 @@ class EventSponsor(models.Model):
 class Hint(models.Model):
     hint = models.AutoField(primary_key=True)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    description = models.CharField(max_length=2048)
+    description = models.TextField(max_length=2048, default='', null=False)
 
 
 class Solution(models.Model):
     solution = models.AutoField(primary_key=True)
     teamuser = models.ForeignKey(TeamUser, on_delete=models.CASCADE)
     eventtask = models.ForeignKey(EventTask, on_delete=models.CASCADE)
-    status = models.BooleanField()
+    status = models.BooleanField(default=None, null=False)
